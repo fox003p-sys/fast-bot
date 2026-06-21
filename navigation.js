7fc5bfb33bfdbf00693bb10371641398fe027039
fox003p-sys <fox003p@gmail.com>
2026-06-21 22:38:32 +0300

    navigation.js

---
+++ navigation.js
@@ -0,0 +1,30 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from './screens/HomeScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import LoginScreen from './screens/LoginScreen';
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+function Tabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+}
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Main" component={Tabs} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
